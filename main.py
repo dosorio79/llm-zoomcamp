6fd6b0258dea9a39da298ca027db57ccefdb65d4
Daniel Osorio <daniel.s.osorio@gmail.com>
2026-06-10 00:40:03 +0100

    Refactor main function to load FAQ data, build index, and generate responses using RAGBase

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from llm-zoomcamp!")
+from ingestion import load_faq_data, build_index
+from rag_helper import RAGBase
+from openai import OpenAI
+from dotenv import load_dotenv
+load_dotenv()
 
+def main():
+    # Load data and build index
+    documents = load_faq_data()
+    index = build_index(documents)
+    
+    # Initialize RAG helper with the index and OpenAI client
+    openai_client = OpenAI()
+
+    assistant = RAGBase(
+        index=index,
+        llm_client=openai_client,
+    )
+    # Get user question and generate response
+    question = input("Please enter your question: ")   
+    answer = assistant.rag(question)
+    print(answer)
 
 if __name__ == "__main__":
     main()
