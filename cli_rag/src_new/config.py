6169969147ccb4dfe7f4a699ae9e973982c22325
Daniel Osorio <daniel.s.osorio@gmail.com>
2026-06-21 21:17:45 +0100

    Add src_new shared scaffolding

---
+++ cli_rag/src_new/config.py
@@ -0,0 +1,16 @@
+# Repository configuration parameters
+REPO_OWNER = "DataTalksClub"
+REPO_NAME = "llm-zoomcamp"
+COMMIT_ID = "8c1834d"
+ALLOWED_EXTENSIONS = {"md"}
+
+# Index configuration parameters
+INDEX_TEXT_FIELDS = ["content"]
+INDEX_KEYWORD_FIELDS = ["filename"]
+
+# Chunking configuration parameters
+CHUNK_SIZE = 2000
+CHUNK_STEP = 1000
+
+# Database configuration parameters
+TEXT_DB_PATH = "storage/chunk.db"
