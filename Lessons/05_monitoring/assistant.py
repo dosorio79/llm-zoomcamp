073e1d5b4a2140ecb9c081a14b717c6af9047e7b
Daniel Osorio <daniel.s.osorio@gmail.com>
2026-07-20 22:35:28 +0100

    Refactor assistant.py: restructure code into notebook format and update imports

--- Lessons/05_monitoring/assistant.py
+++ Lessons/05_monitoring/assistant.py
@@ -1,30 +1,50 @@
-import sys
-
-from dotenv import load_dotenv
-load_dotenv()
-
-sys.path.append("../01_agentic_rag")
-from ingestion import load_faq_data, build_index
-from metrics import RAGWithMetrics
-from openai import OpenAI
-
-def create_assistant():
-    load_dotenv()
-
-    documents = load_faq_data()
-    index = build_index(documents)
-
-    return RAGWithMetrics(
-        index=index,
-        llm_client=OpenAI(),
-    )
-    
-if __name__ == "__main__":
-    assistant = create_assistant()
-
-    query = "How do I join the course?"
-    if len(sys.argv) > 1:
-        query = sys.argv[1]
-
-    answer = assistant.rag(query)
-    print(answer)
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "id": "e1f620e0",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import sys\n",
+    "\n",
+    "from dotenv import load_dotenv\n",
+    "load_dotenv()\n",
+    "\n",
+    "sys.path.append(\"../01_agentic_rag\")\n",
+    "from ingestion import load_faq_data, build_index\n",
+    "from rag_helper import RAGBase\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "996425b2",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "llm-zoomcamp (3.14.5)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.14.5"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
